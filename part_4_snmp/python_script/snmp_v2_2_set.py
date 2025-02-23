23700dacbf31fd376496499d924d522534cdf46e
collinsctk <collinsctk@qytang.com>
2025-02-23 17:36:35 +0800

    初稿

--- part_4_snmp/python_script/snmp_v2_2_set.py
+++ part_4_snmp/python_script/snmp_v2_2_set.py
@@ -6,75 +6,66 @@
 # 教主VIP, 让我们聊点高级的
 # https://vip.qytang.com/
 
-
-from pysnmp.entity.rfc3413.oneliner import cmdgen
-from pysnmp.proto import rfc1902
+import asyncio
+from pysnmp.hlapi.v3arch.asyncio import *
 from snmp_v2_3_getbulk import snmpv2_getbulk
 
 
-def snmpv2_set(ip, community, oid, value, port=161):
-    cmd_gen = cmdgen.CommandGenerator()
-    # print(dir(rfc1902))
-    # 类型 ['ApplicationSyntax', 'Bits', 'Counter32', 'Counter64', 'Gauge32', 'Integer', 'Integer32', 'IpAddress', 'ObjectIdentifier', 'ObjectName', 'ObjectSyntax', 'OctetString', 'Opaque', 'SimpleSyntax', 'TimeTicks', 'Unsigned32', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'constraint', 'error', 'namedtype', 'namedval', 'rfc1155', 'tag', 'univ', 'version_info']
-    # 需要提前通过OIDVIEW查询类型
-    # 通过不同的类型写入数据
+async def snmpv2_set(ip, community, oid, value, port=161):
+    # 根据值的类型，选择相应的 SNMP 类型
     if isinstance(value, str):
-        set_value = rfc1902.OctetString(value)
+        snmp_value = OctetString(value)
     elif isinstance(value, int):
-        set_value = rfc1902.Integer(value)
+        snmp_value = Integer(value)
+    else:
+        raise ValueError(f"不支持的值类型: {type(value)}")
 
-    error_indication, error_status, error_index, var_binds = cmd_gen.setCmd(
-        cmdgen.CommunityData(community),  # 写入Community
-        cmdgen.UdpTransportTarget((ip, port)),  # IP地址和端口号
-                                  (oid, set_value)  # OID和写入的内容，需要进行编码！
-        )
+    # 使用 set_cmd 执行 SNMP SET 操作
+    error_indication, error_status, error_index, var_binds = await set_cmd(
+        SnmpEngine(),
+        CommunityData(community),  # 配置 community
+        await UdpTransportTarget.create((ip, port)),  # 配置目的地址和端口号
+        ContextData(),
+        ObjectType(ObjectIdentity(oid), snmp_value)  # OID 和要设置的值
+    )
+
     # 错误处理
     if error_indication:
-        print("写入错误!!!")
-        print(error_indication)
+        print(f"写入错误!!!\n{error_indication}")
     elif error_status:
-        print("写入错误!!!")
-        print('%s at %s' % (
-            error_status.prettyPrint(),
-            error_index and var_binds[int(error_index) - 1][0] or '?'
-        )
-              )
+        print(f"写入错误!!!\n{error_status} at {error_index and var_binds[int(error_index) - 1][0] or '?'}")
     else:
         print("写入成功!!!")
     # 打印回显示结果
     for name, val in var_binds:
-        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))  # 打印修改的结果
+        print(f'{name.prettyPrint()} = {val.prettyPrint()}')  # 打印修改的结果
 
 
 def get_if_oid(ip, community, if_name):
-    if_result = snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.2", count=25, port=161)
+    if_result = asyncio.run(snmpv2_getbulk(ip, community, "1.3.6.1.2.1.2.2.1.2", count=25, port=161))
     # print(if_result)
     if_result_dict = {}
     for x, y in if_result:
         if_result_dict.update({y: x})
-    # print(if_result_dict)
     if_oid = if_result_dict.get(if_name)
-    if_oid_final = if_oid.replace('SNMPv2-SMI::mib-2.2.2.1.2', '1.3.6.1.2.1.2.2.1.7')
+    if_oid_final = if_oid.replace('1.3.6.1.2.1.2.2.1.2', '1.3.6.1.2.1.2.2.1.7')
     return if_oid_final
 
 
-# 1 为up , 2 为down
+# 1 为 up , 2 为 down
 def shutdown_if(ip, community, if_name, op=1):
     no_shutdown_oid = get_if_oid(ip, community, if_name)
-    snmpv2_set(ip, community, no_shutdown_oid, op)
+    asyncio.run(snmpv2_set(ip, community, no_shutdown_oid, op))
 
 
 if __name__ == "__main__":
-    # 使用Linux解释器 & WIN解释器
-
-    # ip地址与snmp community字符串
-    ip_address = "10.1.1.254"
-    write_community = "Mingjiao.com"
+    # ip 地址与 snmp community 字符串
+    ip_address = "196.21.5.211"
+    write_community = "qytangrw"
     # 设置主机名
-    snmpv2_set(ip_address, write_community, "1.3.6.1.2.1.1.5.0", "SW_L3-1", port=161)
+    asyncio.run(snmpv2_set(ip_address, write_community, "1.3.6.1.2.1.1.5.0", "C8kv1", port=161))
     # shutdown G2
-    # 1 为up , 2 为down
-    snmpv2_set(ip_address, write_community, "1.3.6.1.2.1.2.2.1.7.10", 1, port=161)  # 这个是环回口
-
-    # ~~~根据名称up/down接口~~~
-    shutdown_if(ip_address, write_community, "10GE1/0/5", op=1)
+    # 1 为 up , 2 为 down
+    asyncio.run(snmpv2_set(ip_address, write_community, "1.3.6.1.2.1.2.2.1.7.2", 1, port=161))
+    # 根据名称 up/down 接口
+    shutdown_if(ip_address, write_community, "GigabitEthernet2", op=1)
--- part_4_snmp/python_script/snmp_v2_3_getbulk.py
+++ part_4_snmp/python_script/snmp_v2_3_getbulk.py
@@ -6,71 +6,63 @@
 # 教主VIP, 让我们聊点高级的
 # https://vip.qytang.com/
 
+import asyncio
+from pysnmp.hlapi.v3arch.asyncio import *
 
-from pysnmp.entity.rfc3413.oneliner import cmdgen
 
-
-def snmpv2_getbulk(ip, community, oid, count=25, port=161):
-    cmd_gen = cmdgen.CommandGenerator()
-
-    error_indication, error_status, error_index, var_bind_table = cmd_gen.bulkCmd(
-        cmdgen.CommunityData(community),  # 配置community
-        cmdgen.UdpTransportTarget((ip, port)),  # 配置IP地址和端口号
-        0, count,  # 0为non-repeaters 和  25为max-repetitions(一个数据包中最多25个条目，和显示无关)
-        oid,  # OID
+async def snmpv2_getbulk(ip, community, oid, count=25, port=161):
+    # 使用 bulk_cmd 执行 SNMP GETBULK 操作
+    iterator = bulk_cmd(
+        SnmpEngine(),
+        CommunityData(community),  # 配置 community
+        await UdpTransportTarget.create((ip, port)),  # 配置目的地址和端口号
+        ContextData(),
+        0, count,  # non-repeaters 和 max-repetitions
+        ObjectType(ObjectIdentity(oid)),  # OID
+        lexicographicMode=True
     )
 
-    """
-    non-repeaters介绍
-    the number of objects that are only expected to return a single GETNEXT instance, not multiple instances. Managers \
-    frequently request the value of sysUpTime and only want that instance plus a list of other objects.
-    max-repetitions介绍
-    the number of objects that should be returned for all the repeating OIDs. Agent's must truncate the list to \
-    something shorter if it won't fit within the max-message size supported by the command generator or the agent.
-    详细介绍
-    https://www.webnms.com/snmp/help/snmpapi/snmpv3/snmp_operations/snmp_getbulk.html
-    """
+    # 获取结果
+    error_indication, error_status, error_index, var_binds = await iterator
+
     # 错误处理
     if error_indication:
-        print(error_indication)
+        print(f"读取错误!!!\n{error_indication}")
     elif error_status:
-        print(error_status)
-
-    result = []
-    # varBindTable是个list，元素的个数可能有好多个。它的元素也是list，这个list里的元素是ObjectType，个数只有1个。
-    for var_bind_table_row in var_bind_table:
-        for item in var_bind_table_row:
-            result.append((item.prettyPrint().split("=")[0].strip(), item.prettyPrint().split("=")[1].strip()))
-    return result
-
+        print(f"读取错误!!!\n{error_status} at {error_index and var_binds[int(error_index) - 1][0] or '?'}")
+    else:
+        # 处理返回的 varBinds
+        result = []
+        for var_bind_table_row in var_binds:
+            get_oid = str(var_bind_table_row[0])
+            get_value = str(var_bind_table_row[1])
+            if oid not in get_oid:  # 超过OID范围跳出循环
+                break
+            result.append((get_oid, get_value))
+        return result
 
 if __name__ == "__main__":
-    # 使用Linux解释器 & WIN解释器
-    from pprint import pprint
-    # ip地址与snmp community字符串
-    ip_address = "10.1.1.254"
-    community = "Qytang.com"
+    # ip 地址与 snmp community 字符串
+    ip_address = "196.21.5.211"
+    community = "qytangro"
 
-    # 接口名称
-    raw_name_list = snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.2", port=161)
+    # 获取接口名称
+    raw_name_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.2", port=161))
     if_name_list = [raw_if_name[1] for raw_if_name in raw_name_list]
-    # print(if_name_list)
 
-    # 接口速率
-    raw_speed_list = snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.5", port=161)
+    # 获取接口速率
+    raw_speed_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.5", port=161))
     if_speed_list = [raw_speed[1] for raw_speed in raw_speed_list]
-    # print(if_speed_list)
 
-    # 进接口字节数
-    raw_in_bytes_list = snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.10", port=161)
+    # 获取进接口字节数
+    raw_in_bytes_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.10", port=161))
     if_in_bytes_list = [raw_in_bytes[1] for raw_in_bytes in raw_in_bytes_list]
-    # print(if_in_bytes_list)
 
-    # 出接口字节数
-    raw_out_bytes_list = snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.16", port=161)
+    # 获取出接口字节数
+    raw_out_bytes_list = asyncio.run(snmpv2_getbulk(ip_address, community, "1.3.6.1.2.1.2.2.1.16", port=161))
     if_out_bytes_list = [raw_out_bytes[1] for raw_out_bytes in raw_out_bytes_list]
-    # print(if_out_bytes_list)
 
+    # 汇总接口信息
     interface_list = []
     for name, speed, in_bytes, out_bytes in zip(if_name_list, if_speed_list, if_in_bytes_list, if_out_bytes_list):
         interface_list.append({
@@ -80,4 +72,6 @@ if __name__ == "__main__":
             'out_bytes': out_bytes
         })
 
+    # 打印接口信息
+    from pprint import pprint
     pprint(interface_list)
